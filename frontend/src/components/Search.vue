7daa62b41bbd479d2463f8404dca1b03240d5974
gruffsy <peamoen@gmail.com>
2020-04-02 11:04:10 +0200

    Search

--- frontend/src/components/Search.vue
+++ frontend/src/components/Search.vue
@@ -10,7 +10,43 @@
             v-model="kundeNameSearchString"
           />
         </div>
-
+        <!-- eslint-disable -->
+        <v-text-field 
+        placeholder="Type a name"
+            v-model="kundeNameSearchString"></v-text-field>
+        <v-list-item
+          dense
+          link
+          prepend-icon="mdi-house"
+          three-line
+          router
+          :to="{
+            path: `/customer-objects/${customer.id}`,
+            
+          }"
+           v-for="customer in filteredCustomerFeed"
+            v-bind:key="customer.id"
+        >
+          <!-- eslint-enable -->
+          <v-list-item-content>
+            <v-list-item-icon>
+              <v-icon>mdi-home-city</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title style="font-size: 20px">{{
+              customer.kunde
+            }}</v-list-item-title>
+            <v-list-item-subtitle>
+              {{ customer.badresse }}, {{ customer.bpoststed }}&nbsp;{{
+                customer.bpoststed
+              }}
+            </v-list-item-subtitle>
+            <v-list-item-subtitle
+              >Kontaktperson: {{ customer.kontaktperson }}</v-list-item-subtitle
+            >
+            <v-list-item-subtitle>{{ customer.tlf1 }}</v-list-item-subtitle>
+            <v-list-item-subtitle>{{ customer.tlf2 }}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
         <ul>
           <li
             class="photo"
@@ -42,7 +78,7 @@ export default {
       }
     };
   },
-  
+
   mounted() {
     axios
       .get("api/customers/", this.axiosConfig)
@@ -51,7 +87,7 @@ export default {
       })
       .catch(error => console.log(error));
   },
-  
+
   computed: {
     filteredCustomerFeed: function() {
       var customers = this.customerFeed;
@@ -60,7 +96,7 @@ export default {
       if (!kundeNameSearchString) {
         return customers;
       }
-      
+
       kundeNameSearchString = kundeNameSearchString.trim().toLowerCase();
 
       customers = customers.filter(function(item) {
